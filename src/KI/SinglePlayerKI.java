6133da3b344e0ece66d622740d4b68511777ee04
erik <erikpriemer@gmail.com>
2022-07-03 21:33:04 +0200

    SinglePlayerKI.java deleted

--- src/KI/SinglePlayerKI.java
+++
@@ -1,6 +0,0 @@
-package KI;
-
-import DataManagement.Point;
-
-public class SinglePlayerKI {
-}
--- src/Main.java
+++ src/Main.java
@@ -1,45 +1,12 @@
-import DataManagement.Point;
-import DataManagement.ShipList;
 import Interfaces.StartingGUI;
-import Database.ScoreHandler;
-import DataManagement.GameData;
-
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
+
 
 public class Main {
 
     public static void main(String[] args) throws IOException, ClassNotFoundException {
 
         new StartingGUI();
-
-
-        // Tests
-        /*
-        ShipPlacementKI ki = new ShipPlacementKI(8);
-        ArrayList<Point> i = ki.generateShips(2, 2, 2, 2, 2);
-
-        for(int j = 0; j< i.size(); j++)
-        {
-            System.out.println(i.get(j).getX() + " " + i.get(j).getY());
-        }
-
-         */
-
-
-        /*
-        ScoreHandler s = new ScoreHandler();
-        GameData g = new GameData();
-        g.setTurn(2);
-        int[] j = {1, 2 ,3};
-        g.addMyShip(j);
-        s.serialization(g);
-        GameData v = s.deserialization();
-        int k = v.getTurn();
-        System.out.println(k);
-
-         */
 
     }
 }
